7348ae5c322364385ca2b484b6fc39bd977935b5
Francesco <fpfrancescopalma7@gmail.com>
2016-03-22 16:42:48 +0000

    Adds setup

--- basket.js
+++ basket.js
@@ -1,5 +1,27 @@
 var basket = {
+
   items = {
+    salmon: 4,
+    pizza: 6,
+    bread: 2,
+    milk: 1,
+    eggs: 2,
+    beer: 2
+  },
+  total: function() {
+
+  },
+  add: function() {
+
+  },
+  remove: function() {
+
+  },
+  discount: function() {
+
+  },
+  card: function() {
 
   }
+  
 };
