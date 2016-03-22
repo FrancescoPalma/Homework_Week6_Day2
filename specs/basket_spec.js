7ce653da131ce368543b08f11afabd0149b21c4e
Francesco <fpfrancescopalma7@gmail.com>
2016-03-22 22:14:03 +0000

    Adds initial total, add item, remove item

--- specs/basket_spec.js
+++ specs/basket_spec.js
@@ -3,8 +3,20 @@ var basket = require('../basket');
 
 describe('basket', function(){
 
-  it('should check number of items', function() {
-    assert.equal(5, basket.totalItems)
+  it('should have an initial total of 0', function () {
+    assert.equal(0, basket.total);
+  });
+
+  it('should add an item', function () {
+    basket.cart = [];
+    basket.add('pizza');
+    assert.equal(1, basket.cart.length);
+  });
+
+  it('should remove an item', function () {
+    basket.cart = [];
+    basket.remove('pizza');
+    assert.equal(0, basket.cart.length);
   });
 
 });
